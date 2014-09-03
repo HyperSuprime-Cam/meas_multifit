d7641eb1407beee6157cba52e57b472750724ac2
Paul Price <price@astro.princeton.edu>
2014-09-03 17:16:46 -0400

    config: operate on a SourceMeasurementConfig to consolidate code
    Now call:
    root.calibrate.measurement.load("/path/to/meas_multifit/config/enable.py")
    root.measurement.load("/path/to/meas_multifit/config/enable.py")

--- config/disable.py
+++ config/disable.py
@@ -1,3 +1,6 @@
-root.measurement.algorithms.names.discard("cmodel")
-root.measurement.algorithms.names.discard("multishapelet.psf")
-root.measurement.slots.modelFlux = None
+# Disable CModel measurements
+# The 'root' should be a SourceMeasurementConfig.
+
+root.algorithms.names.discard("cmodel")
+root.algorithms.names.discard("multishapelet.psf")
+root.slots.modelFlux = None
