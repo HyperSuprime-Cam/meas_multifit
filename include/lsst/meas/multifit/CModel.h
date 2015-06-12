08bdaf1504d91f2bd999359fa57c25bae14e5b9c
Paul Price <price@astro.princeton.edu>
2015-06-12 15:56:40 -0400

    optimise parameters for faster CModel while retaining precision
    These parameters, in combination with a change in
    meas_extensions_multiShapelet, result in a speed-up of about a
    factor of two, without compromising the photometric quality.

--- include/lsst/meas/multifit/CModel.h
+++ include/lsst/meas/multifit/CModel.h
@@ -187,13 +187,15 @@ struct CModelControl : public algorithms::AlgorithmControl {
 
     CModelControl() :
         algorithms::AlgorithmControl("cmodel", 2.5),
-        psfName("multishapelet.psf"), minInitialRadius(0.1),
+        psfName("multishapelet.psf"), minInitialRadius(3.0e-3),
         fallbackInitialMomentsPsfFactor(1.5)
     {
         initial.nComponents = 3; // use very rough model in initial fit
         initial.optimizer.gradientThreshold = 1E-2; // with coarse convergence criteria
         initial.optimizer.minTrustRadiusThreshold = 1E-2;
         dev.profileName = "luv";
+        exp.nComponents = 6;
+        exp.optimizer.maxOuterIterations = 250;
     }
 
     PTR(CModelControl) clone() const {
